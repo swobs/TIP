ee2cbb4469f62923e6d2dc98f83b3e4baed33ef9
tw <tw@example.com>
2021-02-19 11:20:03 +0100

    exercise 1: TypeAnalysis, corrected solution

--- src/tip/analysis/TypeAnalysis.scala
+++ src/tip/analysis/TypeAnalysis.scala
@@ -10,18 +10,18 @@ import AstOps._
 import scala.collection.mutable
 
 /**
-  * Unification-based type analysis.
-  * The analysis associates a [[tip.types.Type]] with each variable declaration and expression node in the AST.
-  * It is implemented using [[tip.solvers.UnionFindSolver]].
-  *
-  * To novice Scala programmers:
-  * The parameter `declData` is declared as "implicit", which means that invocations of `TypeAnalysis` obtain its value implicitly:
-  * The call to `new TypeAnalysis` in Tip.scala does not explicitly provide this parameter, but it is in scope of
-  * `implicit val declData: TypeData = new DeclarationAnalysis(programNode).analyze()`.
-  * The TIP implementation uses implicit parameters many places to provide easy access to the declaration information produced
-  * by `DeclarationAnalysis` and the type information produced by `TypeAnalysis`.
-  * For more information about implicit parameters in Scala, see [[https://docs.scala-lang.org/tour/implicit-parameters.html]].
-  */
+ * Unification-based type analysis.
+ * The analysis associates a [[tip.types.Type]] with each variable declaration and expression node in the AST.
+ * It is implemented using [[tip.solvers.UnionFindSolver]].
+ *
+ * To novice Scala programmers:
+ * The parameter `declData` is declared as "implicit", which means that invocations of `TypeAnalysis` obtain its value implicitly:
+ * The call to `new TypeAnalysis` in Tip.scala does not explicitly provide this parameter, but it is in scope of
+ * `implicit val analysisdeclData: TypeData = new DeclarationAnalysis(programNode).analyze()`.
+ * The TIP implementation uses implicit parameters many places to provide easy access to the declaration information produced
+ * by `DeclarationAnalysis` and the type information produced by `TypeAnalysis`.
+ * For more information about implicit parameters in Scala, see [[https://docs.scala-lang.org/tour/implicit-parameters.html]].
+ */
 class TypeAnalysis(program: AProgram)(implicit declData: DeclarationData) extends DepthFirstAstVisitor[Unit] with Analysis[TypeData] {
 
   val log = Log.logger[this.type]()
@@ -31,8 +31,8 @@ class TypeAnalysis(program: AProgram)(implicit declData: DeclarationData) extend
   implicit val allFieldNames: List[String] = program.appearingFields.toList.sorted
 
   /**
-    * @inheritdoc
-    */
+   * @inheritdoc
+   */
   def analyze(): TypeData = {
 
     // generate the constraints by traversing the AST and solve them on-the-fly
@@ -94,40 +94,59 @@ class TypeAnalysis(program: AProgram)(implicit declData: DeclarationData) extend
   }
 
   /**
-    * Generates the constraints for the given sub-AST.
-    * @param node the node for which it generates the constraints
-    * @param arg unused for this visitor
-    */
+   * Generates the constraints for the given sub-AST.
+   * @param node the node for which it generates the constraints
+   * @param arg unused for this visitor
+   */
   def visit(node: AstNode, arg: Unit): Unit = {
     log.verb(s"Visiting ${node.getClass.getSimpleName} at ${node.loc}")
     node match {
-      case program: AProgram => ??? // <--- Complete here
-      case _: ANumber => ??? // <--- Complete here
-      case _: AInput => ??? // <--- Complete here
-      case is: AIfStmt => ??? // <--- Complete here
-      case os: AOutputStmt => ??? // <--- Complete here
-      case ws: AWhileStmt => ??? // <--- Complete here
+      case program: AProgram => // DONE
+        if (program.hasMainFunction) {
+          program.mainFunction.params.foreach(unify(_, IntType()))
+          unify(program.mainFunction.stmts.ret.exp, IntType())
+        }
+      case _: ANumber => unify(_, IntType()) // DONE
+      case _: AInput => unify(_, IntType()) // DONE
+      case is: AIfStmt => unify(is.guard, IntType()) // DONE
+      case os: AOutputStmt => unify(os.exp, IntType()) // DONE
+      case ws: AWhileStmt => unify(ws.guard, IntType()) // DONE
       case as: AAssignStmt =>
         as.left match {
-          case id: AIdentifier => ??? // <--- Complete here
-          case dw: ADerefWrite => ??? // <--- Complete here
-          case dfw: ADirectFieldWrite => ??? // <--- Complete here
-          case ifw: AIndirectFieldWrite => ??? // <--- Complete here
+          case id: AIdentifier => unify(id, as.right) // DONE
+          case dw: ADerefWrite => unify(dw.exp, PointerType(as.right)) //DONE
+          case dfw: ADirectFieldWrite =>
+            unify(dfw.id, RecordType(allFieldNames.map { f =>
+              if (f ==dfw.field) as.right: Term[Type] else FreshVarType()
+            })) // DONE, exercise 3.19
+          case ifw: AIndirectFieldWrite =>
+            unify(ifw.exp, PointerType(RecordType(allFieldNames.map { f =>
+              if (f ==dfw.field) as.right: Term[Type] else FreshVarType()
+            }))) // DONE, exercise 3.19
         }
       case bin: ABinaryOp =>
         bin.operator match {
-          case Eqq => ??? // <--- Complete here
-          case _ => ??? // <--- Complete here
+          case Eqq => // DONE
+            unify(bin.left, bin.right)
+            unify(bin, IntType())
+          case _ => // DONE
+            unify(bin.left, bin.right)
+            unify(bin, bin.left)
+            unify(bin, bin.right)
+            unify(bin, IntType())
+            unify(bin.left, IntType())
+            unify(bin.right, IntType())
+          // TW: choose 2 from 4 = 4 * 3 / 2 = 6
         }
       case un: AUnaryOp =>
         un.operator match {
-          case DerefOp => ??? // <--- Complete here
+          case DerefOp => unify(un.subexp, PointerType(un)) // DONE
         }
-      case alloc: AAlloc => ??? // <--- Complete here
-      case ref: AVarRef => ??? // <--- Complete here
-      case _: ANull => ??? // <--- Complete here
-      case fun: AFunDeclaration => ??? // <--- Complete here
-      case call: ACallFuncExpr => ??? // <--- Complete here
+      case alloc: AAlloc => unify(alloc, PointerType(alloc.exp)) // DONE
+      case ref: AVarRef => unify(ref, PointerType(ref.id)) // DONE
+      case _: ANull => unify(_, PointerType(FreshVarType())) // DONE
+      case fun: AFunDeclaration => unify(fun, FunctionType(fun.params, fun)) // DONE
+      case call: ACallFuncExpr => unify(call.targetFun, FunctionType(call.args, call)) // DONE
       case _: AReturnStmt =>
       case rec: ARecord =>
         val fieldmap = rec.fields.foldLeft(Map[String, Term[Type]]()) { (a, b) =>
